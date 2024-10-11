876c9708e453cbd2e6584e51d8c40d27994aa3c8
Work <work@Nainas-MacBook-Air.local>
2024-10-11 09:45:13 -0700

    adding requests list page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "./Components/Header/Header";
+import RequestList from "./Pages/Request-list/RequestList";
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <RequestList />
+    </div>
+  );
 }
 
 export default App;
